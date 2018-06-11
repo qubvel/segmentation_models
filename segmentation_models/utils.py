e919cdae2b18bfe4637e8e43b80ae16e5d9a3770
Pavel <pavel.yakubovskiy@skolkovotech.ru>
2018-06-11 23:58:11 +0300

    docs

--- segmentation_models/utils.py
+++ segmentation_models/utils.py
@@ -1,5 +1,41 @@
+""" Utility functions for segmentation models """
+
+
 def get_layer_number(model, layer_name):
+    """
+    Help find layer in Keras model by name
+    Args:
+        model: Keras `Model`
+        layer_name: str, name of layer
+
+    Returns:
+        index of layer
+
+    Raises:
+        ValueError: if model does not contains layer with such name
+    """
     for i, l in enumerate(model.layers):
         if l.name == layer_name:
             return i
     raise ValueError('No layer with name {} in  model {}.'.format(layer_name, model.name))
+
+
+def extract_outputs(model, layers, include_top=False):
+    """
+    Help extract intermediate layer outputs from model
+    Args:
+        model: Keras `Model`
+        layer: list of integers/str, list of layers indexes or names to extract output
+        include_top: bool, include final model layer output
+
+    Returns:
+        list of tensors (outputs)
+    """
+    layers_indexes = ([get_layer_number(model, l) if isinstance(l, str) else l
+                      for l in layers])
+    outputs = [model.layers[i].output for i in layers_indexes]
+
+    if include_top:
+        outputs.insert(0, model.output)
+
+    return outputs
