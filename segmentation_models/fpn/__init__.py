225a2c2cd4516085f684e423b387f11bb9813549
Pavel <pavel.yakubovskiy@skolkovotech.ru>
2018-06-12 15:49:45 +0300

    updated __init__.py

---
+++ segmentation_models/fpn/__init__.py
@@ -0,0 +1,14 @@
+from .models import FPNDenseNet121
+from .models import FPNDenseNet169
+from .models import FPNDenseNet201
+from .models import FPNInceptionResNetV2
+from .models import FPNInceptionV3
+from .models import FPNResNet18
+from .models import FPNResNet34
+from .models import FPNResNet50
+from .models import FPNResNet101
+from .models import FPNResNet152
+from .models import FPNResNeXt50
+from .models import FPNResNeXt101
+
+
