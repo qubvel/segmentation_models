8daefb349744c79149b1fc8d50082150b04a2a8a
qubvel <qubvel@gmail.com>
2019-02-02 20:47:20 +0300

    image-classifiers package update

--- segmentation_models/backbones/__init__.py
+++ segmentation_models/backbones/__init__.py
@@ -1,5 +1,17 @@
-from .inception_resnet_v2 import InceptionResNetV2
-from .inception_v3 import InceptionV3
+from classification_models import Classifiers
 
-from .backbones import get_backbone
-from .preprocessing import get_preprocessing
+from . import inception_resnet_v2 as irv2
+from . import inception_v3 as iv3
+
+Classifiers._models.update({
+    'inceptionresnetv2': [irv2.InceptionResNetV2, irv2.preprocess_input],
+    'inceptionv3': [iv3.InceptionV3, iv3.preprocess_input],
+})
+
+
+def get_backbone(name, *args, **kwargs):
+    return Classifiers.get_classifier(name)(*args, **kwargs)
+
+
+def get_preprocessing(name):
+    return Classifiers.get_preprocessing(name)
