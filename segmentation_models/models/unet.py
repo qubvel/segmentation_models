fb29d0c838a5ac2047d710332285b457952ad86a
David Abrahams <40281893+AllWashedOut@users.noreply.github.com>
2020-05-27 16:22:32 -0700

    Partial layer freezing for unet

--- segmentation_models/models/unet.py
+++ segmentation_models/models/unet.py
@@ -186,7 +186,8 @@ def Unet(
             (e.g. ``sigmoid``, ``softmax``, ``linear``).
         weights: optional, path to model weights.
         encoder_weights: one of ``None`` (random initialization), ``imagenet`` (pre-training on ImageNet).
-        encoder_freeze: if ``True`` set all layers of encoder (backbone model) as non-trainable.
+        encoder_freeze: if ``True`` set all layers of encoder (backbone model) as non-trainable. If a float, freezes
+            just that fraction of the encoder layers (starting with the earliest layers)
         encoder_features: a list of layer numbers or names starting from top of the model.
             Each of these layers will be concatenated with corresponding decoder block. If ``default`` is used
             layer names are taken from ``DEFAULT_SKIP_CONNECTIONS``.
@@ -243,7 +244,8 @@ def Unet(
 
     # lock encoder weights for fine-tuning
     if encoder_freeze:
-        freeze_model(backbone, **kwargs)
+        fraction = encoder_freeze if isinstance(encoder_freeze, float) else 1.0
+        freeze_model(backbone, fraction=fraction, **kwargs)
 
     # loading model weights
     if weights is not None:
